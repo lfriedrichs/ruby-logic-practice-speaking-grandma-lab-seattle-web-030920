b9f3b79de21508b4f52f9b3f6f7df76c39f7466c
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-06-28 12:08:37 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(input) 
   if input == 'I LOVE YOU GRANDMA!'
     return 'I LOVE YOU TOO PUMPKIN!' 
-  else if input.match(/[A-Z]/g)
+  else if input.match(/[A-Z]/)
     return 'NO, NOT SINCE 1938!' 
   else 
     'HUH?! SPEAK UP, SONNY!'
